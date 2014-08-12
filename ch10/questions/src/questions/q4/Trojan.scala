f936aaeb230ecdf317045e5fbe9fe11451494830
ludwiggj <graeme.ludwig@btopenworld.com>
2014-08-12 23:27:53 +0100

    [Graeme] Chapter 10, q4

---
+++ ch10/questions/src/questions/q4/Trojan.scala
@@ -0,0 +1,13 @@
+package questions.q4
+
+class Trojan
+
+class Encrypter extends Trojan with Crypto
+
+class DuffDecrypter extends Trojan with Crypto {
+  override val key = -3
+}
+
+class Decrypter extends {
+  override val key = -3
+} with Trojan with Crypto
